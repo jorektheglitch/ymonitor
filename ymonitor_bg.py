ac902b3f4a23cd4d079967bfbe7829a7d4a07dce
jorektheglitch <lozcontrariuz@yandex.ru>
2020-11-03 04:24:23 +0300

    Rewrited background service

--- ymonitor_bg.py
+++ ymonitor_bg.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 from sqlite3 import connect, Cursor, OperationalError
-from socket import create_connection
+from socket import create_connection, error as sock_err
 import json
 from time import sleep
 from datetime import datetime as dt
+from datetime import timezone as tz
 
 
 state = {
@@ -18,8 +20,8 @@ def make_request():
         json.dump(request, y.makefile('w'))
         raw_response = json.load(y.makefile('r'))
         assert isinstance(raw_response, dict)
-    except:
-        return
+    except sock_err:
+        return None
     return raw_response.get('response', {})
 
 def extract_coords(api_response):
@@ -32,7 +34,8 @@ def create_events_table(cursor: Cursor): ...
 def init_db(cursor: Cursor):
     events_table = cursor.execute("""
         CREATE TABLE IF NOT EXISTS events (
-            time text,
+            hour int32,
+            secs int16,
             type int8,
             coords text)""")
 
@@ -40,20 +43,23 @@ def mark_ygg_shutdown(cursor: Cursor):
     if state['yggdrasil_alive']:
         print('[{}] Ygg down'.format(dt.now()))
         state['yggdrasil_alive'] = False
-        time = dt.now()
+        time, secs = divmod(dt.now(tz.utc).timestamp(), 3600)
+        secs = int(secs)
         cursor.execute("""
             INSERT INTO events
-            VALUES (?, ?, ?)""", (str(time), 2, None))
+            VALUES (?, ?, 2, NULL)""", (time*3600, secs))
 
 def coords_event_handler(cursor: Cursor, coords: str):
+    now = dt.now()
     if not state['yggdrasil_alive']:
-        print('[{}] Ygg up'.format(dt.now()))
+        print('[{}] Ygg up'.format(now))
         state['yggdrasil_alive'] = True
-    print("[{}] Coords: [{}]".format(dt.now(), coords))
-    time = dt.now()
+    print("[{}] Coords: [{}]".format(now, coords))
+    time, secs = divmod(dt.now(tz.utc).timestamp(), 3600)
+    secs = int(secs)
     cursor.execute("""
         INSERT INTO events
-        VALUES (?, ?, ?)""", (str(time), 1, coords))
+        VALUES (?, ?, 1, ?)""", (time*3600, secs, coords))
 
 def mainloop(dbname):
     with connect(dbname) as db:
@@ -63,11 +69,14 @@ def mainloop(dbname):
             api_response = make_request()
             if not api_response:
                 mark_ygg_shutdown(c)
+                db.commit()
+                state['coords'] = None
                 sleep(2)
                 continue
             now_coords = extract_coords(api_response)
             if now_coords != state['coords']:
                 coords_event_handler(c, now_coords)
+                db.commit()
                 state['coords'] = now_coords
             sleep(2)
 
