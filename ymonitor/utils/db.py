b18bdc7d6bf88439469f63bbc584078657882bb1
jorektheglitch <lozcontrariuz@yandex.ru>
2021-01-30 16:26:36 +0300

    Fixed SQL queries creating. Minor codestyle fixes.

--- ymonitor/utils/db.py
+++ ymonitor/utils/db.py
@@ -1,7 +1,4 @@
-import asyncio
-from concurrent.futures import ProcessPoolExecutor
-
-from sqlite3 import Connection, Cursor, OperationalError
+from sqlite3 import Connection, OperationalError
 from sqlite3 import connect as connect_db
 
 import time
@@ -40,7 +37,7 @@ class DBWriter:
     VALUES (?, ?, 1, ?)
     """
 
-    def __init__(self, db_name:str=None, connection:Connection=None):
+    def __init__(self, db_name: str = None, connection: Connection = None):
         self.conn = connection or connect_db(db_name)
         self.cursor = self.conn.cursor()
         self.ygg_alive = False
@@ -58,7 +55,7 @@ class DBWriter:
             self.cursor.execute(self.write_shutdown, (hours*3600, secs))
             self.conn.commit()
 
-    def coords_event_handler(self, coords:str):
+    def coords_event_handler(self, coords: str):
         now = dt.now()
         if not self.ygg_alive:
             print('[{}] Ygg up'.format(now))
@@ -68,7 +65,10 @@ class DBWriter:
             print("[{}] Coords: [{}]".format(now, coords))
             hour, secs = divmod(dt.now(tz.utc).timestamp(), 3600)
             secs = int(secs)
-            self.cursor.execute(self.write_coords_change, (hour*3600, secs, coords))
+            self.cursor.execute(
+                self.write_coords_change,
+                (hour*3600, secs, coords)
+            )
             self.conn.commit()
 
 
@@ -77,24 +77,26 @@ class DBReader:
     by_hour_query = """
     SELECT hour, count(*) as shifts
     FROM events
-    WHERE type==1 AND hour BETWEEN {} AND {}
+    WHERE type==1 AND hour BETWEEN ? AND ?
     GROUP BY hour
     """
     all_query = """
     SELECT hour+secs as time
     FROM events
-    WHERE type==1 AND time BETWEEN {} AND {}
+    WHERE type==1 AND time BETWEEN ? AND ?
     """
 
     def __init__(self, dbname):
         self.dbname = dbname
 
-    def ask_db(self, query, retries=5, delay=0.5)->List[Tuple[str, str]]:
+    def ask_db(
+        self, query, params=(), retries=5, delay=0.5
+    ) -> List[Tuple[str, str]]:
         with connect_db(self.dbname) as conn:
             cursor = conn.cursor()
             for _ in range(retries):
                 try:
-                    response = cursor.execute(query).fetchall()
+                    response = cursor.execute(query, params).fetchall()
                 except OperationalError as err:
                     last_error = err
                     time.sleep(delay)
@@ -105,30 +107,17 @@ class DBReader:
             cursor.close()
         return response
 
-    def _format_template(self, template:str, end:dt, interval:int):
-        delta = td(hours=interval)
+    def _get_interval(self, interval: str):
+        hours = Intervals[interval].value
+        end = dt.now(tz.utc)
+        delta = td(hours=hours)
         start = end - delta
-        return template.format(
-            start.timestamp(),
-            end.timestamp()
-        )
+        return start.timestamp(), end.timestamp()
 
-    def get_by_hours(self, interval:str):
-        hours = Intervals[interval].value
-        now_time = dt.now(tz.utc)
-        query = self._format_template(
-            self.by_hour_query,
-            now_time,
-            hours
-        )
-        return self.ask_db(query)
+    def get_by_hours(self, delta: str):
+        interval = self._get_interval(delta)
+        return self.ask_db(self.by_hour_query, interval)
 
-    def get_all(self, interval:str):
-        hours = Intervals[interval].value
-        now_time = dt.now(tz.utc)
-        query = self._format_template(
-            self.all_query,
-            now_time,
-            hours
-        )
-        return self.ask_db(query)
+    def get_all(self, delta: str):
+        interval = self._get_interval(delta)
+        return self.ask_db(self.all_query, interval)
